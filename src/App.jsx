f152e2b958ff15d298fae9be7a2ae4aeec8f5e41
Anoushka Das <rinkyanoushka@example.com>
2026-03-17 21:22:32 -0500

    web project check in 1

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>CS571 Web Project</h1>
+      <p>Check-In #1</p>
+      <p>By Anoushka, Triya, and Ahaan</p>
+    </div>
+  );
+}
+
+export default App;
